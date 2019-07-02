54d09eed0634258d1648754f2892586848493dbc
Brody Currie <brodycurrie@gmail.com>
2019-07-02 13:41:36 -0400

    completed reinforcement exercise

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function arrayToObj() {
-   // your code goes here  
-}
+function arrayToObj(array) {
+   return array.reduce((prevValue, currVal) => {
+      prevValue[currVal[0]] = currVal[1];
+      return prevValue;
+   }, {});
+};
 
 module.exports = arrayToObj;
